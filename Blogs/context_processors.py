a6ca39c664afa65b215410f30481b1ec72d8d087
Dheerajll <xettridheeraj01@gmail.com>
2026-01-04 21:26:12 +0545

    Added socials to context processor.

--- Blogs/context_processors.py
+++ Blogs/context_processors.py
@@ -1,5 +1,11 @@
 from .models import *
-
+from assignments.models import Socials
 def get_categories(request):
     categories = Category.objects.all()
     return dict(categories=categories)
+
+
+
+def get_socials(request):
+    socials = Socials.objects.all()
+    return dict(socials=socials)
