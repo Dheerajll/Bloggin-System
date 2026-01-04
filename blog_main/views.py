c7e6a15eb42ab215cb7c85c73d26cffda89420af
Dheerajll <xettridheeraj01@gmail.com>
2026-01-04 10:52:23 +0545

    Sent the blog context from database to home template.

--- blog_main/views.py
+++ blog_main/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from Blogs.models import Category,Blog
 def home(request):
-    return render(request,'home.html')
+    categories = Category.objects.all()
+    featured_post = Blog.objects.filter(is_featured = True,status = 'Published')
+    posts = Blog.objects.filter(is_featured = False,status = 'Published')
+    context = {
+        'categories':categories,
+        'featured_post':featured_post,
+        'posts':posts
+    }
+    return render(request,'home.html',context)
