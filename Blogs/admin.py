fe4f49d530edcaf75cb6e2ada3dd74d76d49fdb1
Dheerajll <xettridheeraj01@gmail.com>
2026-01-04 09:19:42 +0545

    Added some admin panel features.

--- Blogs/admin.py
+++ Blogs/admin.py
@@ -3,6 +3,9 @@ from .models import *
 # Register your models here.
 class BlogAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug':('title',)} # adding this generates the slug based on the title
+    list_display = ['title','category','author','status','is_featured']
+    search_fields = ('id','title','category__category_name','status') # search field that helps find particular blog in admin panel based on given parameters
+    list_editable = ('is_featured',)
 
 
 admin.site.register(Category)
