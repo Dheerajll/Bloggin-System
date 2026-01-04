04c2e5868b4f043e14982f35be8cac6475a500c8
Dheerajll <xettridheeraj01@gmail.com>
2026-01-04 18:12:07 +0545

    Made a new url path for single blog pages.

--- blog_main/urls.py
+++ blog_main/urls.py
@@ -19,12 +19,13 @@ from django.urls import path,include
 from django.conf.urls.static import static
 from django.conf import settings
 
-
+from Blogs import views as Blogviews
 from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.home,name='home'),
     path('category/',include('Blogs.urls')),
+    path('<slug:slug>',Blogviews.blog,name='blog')
 
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- blog_main/views.py
+++ blog_main/views.py
@@ -5,7 +5,7 @@ from Blogs.models import Category,Blog
 def home(request):
     categories = Category.objects.all()
     featured_post = Blog.objects.filter(is_featured = True,status = 'Published')
-    posts = Blog.objects.filter(is_featured = False,status = 'Published')
+    posts = Blog.objects.filter(is_featured = False,status = 'Published').order_by('-created_at')
     context = {
         'categories':categories,
         'featured_post':featured_post,
