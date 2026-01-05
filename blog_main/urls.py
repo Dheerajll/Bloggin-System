159e014b190a880bd98269d80bbbd21d341e8a60
Dheerajll <xettridheeraj01@gmail.com>
2026-01-05 18:01:27 +0545

    Added custom styling in pages.

--- blog_main/urls.py
+++ blog_main/urls.py
@@ -18,8 +18,6 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf.urls.static import static
 from django.conf import settings
-
-from Blogs import views as Blogviews
 from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -28,6 +26,7 @@ urlpatterns = [
     path('login/',views.login_page,name='login'),
     path('logout/',views.logout_page,name='logout'),
     path('blogs/',include('Blogs.urls')),
+    path('dashboard/',include('dashboards.urls')),
 
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
