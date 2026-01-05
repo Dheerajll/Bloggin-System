38f43ec01f392d31ce75e654e95030abfc009af0
Dheerajll <xettridheeraj01@gmail.com>
2026-01-05 07:58:28 +0545

    Added search function.

--- Blogs/views.py
+++ Blogs/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,redirect,get_object_or_404
 from django.http import HttpResponse
+from django.db.models import Q
 from .models import *
 # Create your views here.
 def posts_by_category(request,category_id):
@@ -21,3 +22,14 @@ def blog(request,slug):
         'blog':post
     }
     return render(request,'blog.html',context)
+
+def search(request):
+    keyword = request.GET['keyword']
+    blogs = Blog.objects.filter(Q(title__icontains=keyword) | Q(short_description__icontains=keyword) | 
+                                Q(blog_body__icontains=keyword) | Q(author__username__icontains = keyword) | 
+                                Q(category__category_name__icontains = keyword), status = 'Published')
+    context = {
+        'blogs':blogs,
+        'keyword':keyword
+    }
+    return render(request,'search.html',context)
--- blog_main/urls.py
+++ blog_main/urls.py
@@ -25,7 +25,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.home,name='home'),
     path('category/',include('Blogs.urls')),
-    path('<slug:slug>',Blogviews.blog,name='blog')
+    path('<slug:slug>',Blogviews.blog,name='blog'),
+    path('blogs/search/',Blogviews.search,name='search'),
 
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
